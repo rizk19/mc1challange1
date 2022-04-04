642190596754c0533680a1ec29e5b1d9ada902ad
Rizki Ahmad Faris <rizkiahf@gmail.com>
2022-04-04 07:00:51 +0700

    Initial Commit

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Rizki Faris on 04/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
